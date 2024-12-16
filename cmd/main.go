e47b8e5d5536764e575adfc9f6319703d7a32e99
jeebuscrossaint <thediamond270@gmail.com>
2024-12-16 10:22:32 -0500

    version and root checker

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"os/user"
+	"runtime"
         "tuinit/pkg/initsys"
 )
 
+const (
+	Version = "0.1.0"
+)
+
+func checkRoot() bool {
+	currentUser, err := user.Current()
+	if err != nil {
+		fmt.Println("Error getting current user: ", err)
+		os.Exit(1)
+	}
+	return currentUser.Uid == "0"
+}
+
+func printVersion() {
+	fmt.Printf("tuinit version %s\n", Version)
+	fmt.Printf("Go version: %s\n", runtime.Version())
+	// add other dependencies versions here
+	os.Exit(0)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Is systemd? ", initsys.IsSystemd())
+	// define flags
+	version := flag.Bool("version", false, "print version information")
+	flag.Parse()
+
+	// handle version flag
+	if *version {
+		printVersion()
+	}
+
+	// check if running as root
+	if !checkRoot() {
+		fmt.Println("tuinit must be run as root")
+		os.Exit(1)
+	}
+
+	fmt.Println("tuinit is running as root")
+	initsys.IsSystemd()
 }
