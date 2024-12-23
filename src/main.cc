314c74d30c932f9d763457a25ca584427acb1a8a
jeebuscrossaint <thediamond270@gmail.com>
2024-12-23 18:22:54 -0500

    hello world rn

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,16 @@
 //
 // Created by amarnath on 12/22/24.
 //
+
+#include <qt6/QtWidgets/QApplication>
+#include <qt6/QtWidgets/QPushButton>
+
+int main(int argc, char *argv[]) {
+        QApplication app(argc, argv);
+
+        QPushButton button("Hello World");
+        button.resize(200, 100);
+        button.show();
+
+        return app.exec();
+}
