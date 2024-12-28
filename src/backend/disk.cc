f4e7093ed385ee7caec950963fddc743811400ab
jeebuscrossaint <thediamond270@gmail.com>
2024-12-28 13:56:56 -0500

    some stuff

--- src/backend/disk.cc
+++ src/backend/disk.cc
@@ -4,3 +4,26 @@
 
 #include "disk.hh"
 
+DISK::DISK() {};
+DISK::~DISK() {};
+
+std::vector<std::string> DISK::getAllDisks() {
+        std::vector<std::string> disks;
+        std::ifstream disksFile("/proc/partitions");
+
+        if (disksFile.is_open()) {
+                std::string line;
+                while (std::getline(disksFile, line)) {
+                        // Extract disk names using regex
+                        std::smatch match;
+                        if (std::regex_search(line, match, std::regex("\\b(sd[a-z]|hd[a-z]|nvme[0-9]n[0-9])\\b"))) {
+                                disks.push_back(match[1]);
+                        }
+                }
+                disksFile.close();
+        } else {
+                std::cerr << "Error opening /proc/partitions" << std::endl;
+        }
+
+        return disks;
+}
--- src/backend/disk.hh
+++ src/backend/disk.hh
@@ -5,6 +5,23 @@
 #ifndef SYSTRA_DISK_HH
 #define SYSTRA_DISK_HH
 
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <regex>
 
+class DISK {
+public:
+	DISK();
+        ~DISK();
+
+protected:
+	uint64_t getDiskUsage();
+	uint64_t getDiskFreeUsage();
+        std::vector<std::string> getAllDisks();
+
+private:
+	std::vector<std::string> diskNames;
+};
 
 #endif //SYSTRA_DISK_HH
